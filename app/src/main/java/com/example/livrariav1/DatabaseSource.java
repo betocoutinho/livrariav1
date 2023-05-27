2161b65d63fb7d3d698ffec9755634841a9fd6bf
betocoutinho <robertogmcfilho@gmail.com>
2023-05-27 11:32:02 -0300

    1 - Configuração do banco de dados 2 - Tela para adicionar o livro

---
+++ app/src/main/java/com/example/livrariav1/DatabaseSource.java
@@ -0,0 +1,47 @@
+package com.example.livrariav1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseSource  extends SQLiteOpenHelper {
+
+    private Context context;
+
+    //Configuração do banco
+    private static final String DATABASE_NAME = "livraria.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //configuração da tabela
+    private static final String TABLE_NAME = "livros";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITULO = "titulo";
+    private static final String COLUMN_AUTOR = "autor";
+    private static final String COLUMN_PAGINAS = "paginas";
+
+
+
+    public DatabaseSource(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_TITULO + " TEXT, " +
+                        COLUMN_AUTOR + " TEXT, " +
+                        COLUMN_PAGINAS + " INTEGER);";
+
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
